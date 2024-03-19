ac343beeb7cc2ebdf77b2da5eab7e8acf5516f52
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-03-19 15:11:24 +0900

    3월 19일 화요일(3주 1일)

--- STL.cpp
+++ STL.cpp
@@ -1,18 +1,19 @@
 //------------------------------------------------------------------------
 // 2024.	1학기 STL 화56목56      3월 19일 화요일			(3주 1일)
 // 
-// FILE I/O text/binary - high/low level
+// 실행파일의 메모리 구조 - 찾아보세요: STACK CODE DATA Free Store
 //------------------------------------------------------------------------
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <algorithm>
 #include "save.h"
 //using namespace std;	// 사용하지 않고 코딩
 
 // [문제] "개들"에는 class Dog 객체 100개가 기록되어 있다.
 // 파일은 binary 모드이고, write 함수로 모든 객체를 한 번의 write()로 기록하였다.
-// 파일을 읽어 가장 num 값이 큰 Dog 정보를 화면에 출력하라.
+// 파일을 읽어 num 값이 1'0000 미만인 Dog 객체는 몇 개인지 화면에 출력하라.
 // class Dog의 멤버는 다음과 같다.
 
 class Dog {
@@ -45,15 +46,15 @@ int main()
 	}
 	
 	std::array<Dog, 100> dogs;
-	in.read(reinterpret_cast<char*>(dogs.data()), sizeof(Dog)*100);
+	in.read((char*)(dogs.data()), sizeof(Dog)*100);
 
-	auto pos = std::max_element(dogs.begin(), dogs.end(), [](const Dog& a, const Dog& b) {
-		return a.getNum() < b.getNum();
+	int num = std::count_if(dogs.begin(), dogs.end(), [](const Dog& dog) {
+		if (1'0000 > dog.getNum())
+			return true;
+		return false;
 		});
 
-	std::cout << "num 값이 가장 큰 Dog" << std::endl;
-	pos->Show();	// (*pos).Show()와 똑같지만, 이렇게 쓰면 안됨.
-	// Dog, c-y, num-9929
+	std::cout << "개수: " << num << std::endl;
 
 	save("STL.cpp");
 }
