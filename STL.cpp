5f051e54f5ca811613edc15febc4834cc3fcfff5
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-04-09 15:10:41 +0900

    0409_6주1일

--- STL.cpp
+++ STL.cpp
@@ -1,12 +1,14 @@
 //------------------------------------------------------------------------
-// 2024.	1학기 STL 화56목56      4월 4일 목요일			(5주 2일)
+// 2024.	1학기 STL 화56목56      4월 9일 화요일			(6주 1일)
 // 
 // 4월 25일 (8주 1일) - 중간시험
+// 
+// STL Container - Containers are objects that store other objects.
 //------------------------------------------------------------------------
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <array>
-#include <algorithm>
+#include <fstream>
 #include "save.h"
 #include "String.h"
 //using namespace std;	// 사용하지 않고 코딩
@@ -17,22 +19,16 @@ extern bool 관찰;
 int main()
 //--------
 {
-	std::array<String, 5> a{
-		"12341232312312312312312314123999999999",
-		"23313143423123213243245324567463214567463213`3243565333",
-		"213333333333333424121",
-		"777777713231232312323123",
-		"555551323213123213"
-	};
+	std::array<String, 50> a;
 
-	관찰 = true;
-	// [문제] a의 각 String이 관리하는 글자를 오름차순으로 정렬하라.
-	for (const String& s : a)
-		std::sort(s.getData(), s.getData() + s.getLen());
-	관찰 = false;
+	// [문제] "STL.cpp"의 단어를 a에 저장하라.
+	// 길이 오름차순으로 a를 정렬하라.
+	// 화면에 출력하라.
 
-	for (String& s : a)
-		std::cout << s << '\n';
-	
+	std::ifstream in{ "STL.cpp" };
+
+	for (int i = 0; i < 50; ++i)
+		in >> a[i];		// in >> std::string 을 이용해서 하자
+
 	save("STL.cpp");
 }
