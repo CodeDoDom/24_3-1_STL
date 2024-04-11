d1065a76da135509cbcd54c9d67c7cfb49dccfe1
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-04-11 15:10:35 +0900

    0411_6주2일

--- STL.cpp
+++ STL.cpp
@@ -1,14 +1,17 @@
 //------------------------------------------------------------------------
-// 2024.	1학기 STL 화56목56      4월 9일 화요일			(6주 1일)
+// 2024.	1학기 STL 화56목56      4월 11일 목요일			(6주 2일)
 // 
 // 4월 25일 (8주 1일) - 중간시험
 // 
-// STL Container - Containers are objects that store other objects.
+// STL container - Sequence - vector(dynamic array - [] operator)
+//	array는 continuos data다.
+//	dynamic <=> static
+//	dynamic := run time
 //------------------------------------------------------------------------
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <array>
-#include <fstream>
+#include <vector>
+#include <ranges>
 #include "save.h"
 #include "String.h"
 //using namespace std;	// 사용하지 않고 코딩
@@ -19,16 +22,13 @@ extern bool 관찰;
 int main()
 //--------
 {
-	std::array<String, 50> a;
+	관찰 = true;
 
-	// [문제] "STL.cpp"의 단어를 a에 저장하라.
-	// 길이 오름차순으로 a를 정렬하라.
-	// 화면에 출력하라.
+	// [문제] 키보드에서 입력한 int 값의 합계와 평균값을 출력하라.
+	std::vector<int> v{ std::istream_iterator<int>{std::cin}, {} };
 
-	std::ifstream in{ "STL.cpp" };
-
-	for (int i = 0; i < 50; ++i)
-		in >> a[i];		// in >> std::string 을 이용해서 하자
+	for (int num : v)
+		std::cout << num << " ";
 
 	save("STL.cpp");
 }
