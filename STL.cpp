314e419a8e35dc19a50907a525c7c53c56713f1c
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-04-16 15:04:32 +0900

    0416_7주1일

--- STL.cpp
+++ STL.cpp
@@ -1,17 +1,18 @@
 //------------------------------------------------------------------------
-// 2024.	1학기 STL 화56목56      4월 11일 목요일			(6주 2일)
+// 2024.	1학기 STL 화56목56      4월 16일 목요일			(7주 1일)
 // 
-// 4월 25일 (8주 1일) - 중간시험
+// 4월 25일 (8주 2일) - 중간시험
 // 
 // STL container - Sequence - vector(dynamic array - [] operator)
-//	array는 continuos data다.
-//	dynamic <=> static
-//	dynamic := run time
+// 
+// push_back(T) - amortized 0(1)
 //------------------------------------------------------------------------
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
-#include <ranges>
+#include <array>
+#include <print>
+#include <numeric>		// 자주 사용해보세요
 #include "save.h"
 #include "String.h"
 //using namespace std;	// 사용하지 않고 코딩
@@ -22,13 +23,13 @@ extern bool 관찰;
 int main()
 //--------
 {
-	관찰 = true;
-
-	// [문제] 키보드에서 입력한 int 값의 합계와 평균값을 출력하라.
-	std::vector<int> v{ std::istream_iterator<int>{std::cin}, {} };
-
-	for (int num : v)
-		std::cout << num << " ";
+	// [문제] "STL.cpp"에 있는 소문자의 출현 횟수를 다음과 같이 출력하라.
+	// a - 10
+	// b - 3
+	// c - 0
+	// (생략)
+	// z - 2
+	// (15분 이내에 풀 수 있어야 한다고 함)
 
 	save("STL.cpp");
 }
