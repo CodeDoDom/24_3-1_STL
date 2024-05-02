efcd0d36ca5218204dcefb20087377c09d34f78d
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-05-02 15:28:10 +0900

    0502_9주2일

--- STL.cpp
+++ STL.cpp
@@ -1,33 +1,42 @@
 //------------------------------------------------------------------------
-// 2024.	1학기 STL 화56목56      4월 30일 화요일			(9주 1일)
+// 2024.	1학기 STL 화56목56      5월 2일 목요일			(9주 2일)
 // 
-// list
+// 반복자: iterator - 반복자는 포인터를 일반화한 것이다.
+//  
+// Iterators are a generalization of pointers 
+// that allow a C++ program to work with different data structures
+// in a uniform manner.
 //------------------------------------------------------------------------
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <vector>
+#include <forward_list>
 #include <list>
-#include <fstream>
+#include <deque>
 #include "save.h"
 #include "String.h"
 //using namespace std;	// 사용하지 않고 코딩
 
 extern bool 관찰;
 
+template<class 반복자>
+void f(반복자 iter)
+{
+	std::cout << typeid(반복자::iterator_category).name() << std::endl;
+}
+
 //--------
 int main()
 //--------
 {
-	// [문제] 파일 "STL.cpp"의 단어를 읽어 정렬한 후 출력하라.
-	std::ifstream in("STL.cpp");
+	// 클래스 String은 contiguous 메모리를 관리한다.
+	// 다음 코드가 실행되게 하라.
 
-	std::list<String> cont{ std::istream_iterator<String>{in}, {} };
+	String s{ "0123456789" };
 
-	cont.sort();
-
-	cont.unique();		// 중복된 원소를 삭제한다.
-
-	for (const String& s : cont)
-		std::cout << s << std::endl;
+	//for (char s : s)				// begin, end를 제공해야 함.
+	//	std::cout << s << ' ';
+	//std::cout << std::endl;
 
 	save("STL.cpp");
 }
