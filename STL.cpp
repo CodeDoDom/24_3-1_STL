b61c21212a916774cfd6725b65f528f59edc44a6
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-05-14 15:32:44 +0900

    0514_11주1일

--- STL.cpp
+++ STL.cpp
@@ -1,43 +1,44 @@
 //------------------------------------------------------------------------
-// 2024.	1학기 STL 화56목56      5월 9일 목요일			(10주 2일)
+// 2024.	1학기 STL 화56목56      5월 14일 화요일			(11주 1일)
 // 
-// 반복자: iterator - 반복자는 포인터를 일반화한 것이다.
+// 알고리즘 함수 - 반복자만 인자로 받는다.
 //  
-// 6월 6일 목(현충일): 강의 예정
+// 6월  6일 목(현충일): 강의 예정
+// 6월 13일 목(15주 2일): 기말 시험
 //------------------------------------------------------------------------
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <vector>
 #include "save.h"
 #include "String.h"
 //using namespace std;	// 사용하지 않고 코딩
 
 extern bool 관찰;
 
-template <class 반복자, class 호출가능타입>
-반복자 my_find(반복자 b, 반복자 e, 호출가능타입 f)
+template <class InIter, class Outlter>
+void my_copy(InIter b, InIter e, Outlter d )
 {
-
+	while(b != e){
+		*d = *b;
+		++b;
+		++d;
+	}
 }
 
 //--------
 int main()
 //--------
 {
-	String s{ "1357924680" };
-	
-	// [문제] 다음 코드가 문제없이 실행되도록 하자.
-	// 이 코드는 [begin(), end())에서 주어진 조건에 맞는 원소의 위치를 리턴한다.
-
-	auto p = my_find(s.begin(), s.end(), [](char c) {
-		if (std::stoi(c) % 2 == 0)
-			return true;
-		return false;
-		});
-
-	std::cout << "처음 찾은 짝수의 위치는 " << std::endl;
-	std::cout << "처음 찾은 짝수의 값은 " << std::endl;
-
 	save("STL.cpp");
+
+	String s{ "STL algorithm 2024년 5월 14일" };
+	std::vector<char> v;
+
+	my_copy(s.begin(), s.end(),  std::back_inserter(v));
+	
+	for (char c : v)
+		std::cout << c;
+	std::cout << std::endl;
 }
