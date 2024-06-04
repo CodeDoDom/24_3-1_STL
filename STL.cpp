7efad45e903ddeed032df7eedbceab8183fa472a
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-06-04 15:21:02 +0900

    0604_14주1일

--- STL.cpp
+++ STL.cpp
@@ -1,57 +1,29 @@
 //------------------------------------------------------------------------
-// 2024.	1학기 STL 화56목56      5월 30일 목요일			(13주 2일)
+// 2024.	1학기 STL 화56목56      6월 04일 화요일			(14주 1일)
 //  
 // 6월  6일 목(현충일): 강의 예정
 // 6월 13일 목(15주 2일): 기말 시험
 // 
-// 정렬관련 Algorithm - 복잡도 순으로
-// partition			- 조건에 따라 분리
-// nth_element			- n등까지와 나머지로 분리
-// partial_sort			- n등까지는 정렬된 상태로 나머지는 무관
-// sort					- 전체 정렬
-// stable_sort
+// distance 구현
 //------------------------------------------------------------------------
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <array>
+#include <string>
+#include <list>
 #include <algorithm>
-#include <print>
-#include <random>
+#include <ranges>
 #include "save.h"
 #include "String.h"
-//using namespace std;	// 사용하지 않고 코딩
+using namespace std;	// 사용하지 않고 코딩
 
 extern bool 관찰;
-
-std::random_device rd;
-std::default_random_engine dre{rd()};
-std::uniform_int_distribution uid{ 1, 50 };
-//std::uniform_int_distribution uidC{ 'A', 'Z' };
-
-struct Dog {
-	char c;
-	int n;
-
-	Dog() {
-		//c = uidC(dre);
-		n = uid(dre);
-	}
-};
 
 //--------
 int main()
 //--------
 {
-	// stable_sort
-
-	std::array<Dog, 100> dogs;
-
-	std::sort(dogs.begin(), dogs.end(), [](const Dog& a, const Dog& b) {
-		return a.c < b.c;
-		});
-
-	for (auto [글자, 숫자] : dogs)
-		std::cout << 글자 << " - " << 숫자 << std::endl;
-
+	std::list<int> list{ 1,2,3,4 };
+	std::ranges::sort(list.begin(), list.end());
+	
 	save("STL.cpp");
 }
