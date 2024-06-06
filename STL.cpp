ce7f984f977bc21c1443ea1686de71dac47dedda
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-06-06 15:11:28 +0900

    0606_14주2일

--- STL.cpp
+++ STL.cpp
@@ -1,29 +1,63 @@
 //------------------------------------------------------------------------
-// 2024.	1학기 STL 화56목56      6월 04일 화요일			(14주 1일)
+// 2024.	1학기 STL 화56목56      6월 06일 목요일			(14주 2일)
 //  
 // 6월  6일 목(현충일): 강의 예정
 // 6월 13일 목(15주 2일): 기말 시험
 // 
-// distance 구현
+// C++20 Concept / Range
+// 
+// for/accumulate/reduce
 //------------------------------------------------------------------------
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
-#include <list>
 #include <algorithm>
-#include <ranges>
+#include <array>
+#include <random>
+#include <chrono>
+#include <numeric>
+#include <execution>
 #include "save.h"
 #include "String.h"
-using namespace std;	// 사용하지 않고 코딩
+// using namespace std;	// 사용하지 않고 코딩
 
-extern bool 관찰;
+std::default_random_engine dre;
+std::uniform_int_distribution uid;
+
+std::array<int, 2'5000'0000> a;
+
+// for - loop
+// 걸린 시간: 141ms
+// 합계: 268416090773281637
+
+// 알고리즘 - accumulate
+// 걸린 시간 : 165ms
+// 합계 : 268416090773281637
+
+// 알고리즘 reduce
+// 걸린 시간 : 79ms
+// 합계 : 268416090773281637
+
+// 알고리즘 - reduce(execution::par)
+// 걸린 시간 : 61ms
+// 합계 : 268416090773281637
 
 //--------
 int main()
 //--------
 {
-	std::list<int> list{ 1,2,3,4 };
-	std::ranges::sort(list.begin(), list.end());
+	for (int& num : a)
+		num = uid(dre);
 	
+	// 합산
+	long long sum{};
+
+	auto b = std::chrono::high_resolution_clock::now();
+	
+	sum = std::reduce(std::execution::par, a.begin(), a.end(), 0LL);
+
+	auto d = std::chrono::high_resolution_clock::now() - b;
+	std::cout << "걸린 시간: " << std::chrono::duration_cast<std::chrono::milliseconds>(d) << std::endl;
+	std::cout << "합계: " << sum << std::endl;
+
 	save("STL.cpp");
 }
